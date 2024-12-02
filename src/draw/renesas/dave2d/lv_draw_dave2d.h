ff1e25fc00e38a5e46a33c9afe8ea653f437d9b5
Duy Nguyen <duy.nguyen.xa@renesas.com>
2024-12-02 13:30:47 +0700

    fix(dave2d): Fix build warning and porting for Zephyr
    Temporary fix build issue and porting for ZephyrRTOS
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

--- src/draw/renesas/dave2d/lv_draw_dave2d.h
+++ src/draw/renesas/dave2d/lv_draw_dave2d.h
@@ -26,6 +26,8 @@ extern "C" {
 #include "../../lv_draw_image.h"
 #include "../../lv_draw_triangle.h"
 #include "../../lv_draw_buf.h"
+#include "../../lv_draw_image_private.h"
+#include "../../../misc/lv_area_private.h"
 
 
 /*********************
