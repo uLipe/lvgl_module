f5cdae2fbc2322cf8bb89669718a5b9438d02bd2
Felipe Neves <ryukokki.felipe@gmail.com>
2025-07-29 21:09:16 -0300

    lib: nema: add the cm55 core library
    Signed-off-by: Felipe Neves <ryukokki.felipe@gmail.com>

--- libs/nema_gfx/include/nema_blender.h
+++ libs/nema_gfx/include/nema_blender.h
@@ -68,7 +68,7 @@ extern "C" {
 
 
 #define NEMA_BLOP_NONE         (0U)           /**< No extra blending operation */
-#define NEMA_BLOP_RECOLOR      (0x00100000U)  /**< Cconst*Aconst + Csrc*(1-Aconst). Overrides MODULATE_RGB. On NemaP GPU, recolor is available only when HW Rop Blender is enabled */
+//#define NEMA_BLOP_RECOLOR      (0x00100000U)  /**< Cconst*Aconst + Csrc*(1-Aconst). Overrides MODULATE_RGB. On NemaP GPU, recolor is available only when HW Rop Blender is enabled */
 #define NEMA_BLOP_LUT          (0x00200000U)  /**< src_tex as index, src2_tex as palette */
 #define NEMA_BLOP_STENCIL_XY   (0x00400000U)  /**< Use TEX3 as mask */
 #define NEMA_BLOP_STENCIL_TXTY (0x00800000U)  /**< Use TEX3 as mask */
